f92c101a3eba338a89b28715fe214b82e1ec4999
Aleksandr Lossenko <aleksandr.lossenko.extern@wlw.de>
2019-12-01 10:20:11 +0100

    small improvements

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -9,30 +9,28 @@ defmodule Day1 do
   def part1(file_path) do
     file_path
     |> file_stream()
-    |> Stream.map(fn mass ->
-      div(mass, 3) - 2
-    end)
+    |> Stream.map(&needed_fuel/1)
     |> Enum.sum()
   end
 
   def part2(file_path) do
     file_path
     |> file_stream()
-    |> Stream.map(&calculate_fuel/1)
+    |> Stream.map(&total_fuel/1)
     |> Enum.sum()
   end
 
-  def calculate_fuel(mass) do
-    do_calculate_mass(mass, [])
+  defp total_fuel(mass, total \\ 0)
+  defp total_fuel(mass, total) when mass <= 0, do: total
+
+  defp total_fuel(mass, total) do
+    fuel = needed_fuel(mass)
+
+    total_fuel(fuel, total + fuel)
   end
 
-  def do_calculate_mass(mass, acc) when mass <= 0, do: Enum.sum(acc)
-
-  def do_calculate_mass(mass, acc) do
-    new_mass =
-      (div(mass, 3) - 2)
-      |> max(0)
-
-    do_calculate_mass(new_mass, [new_mass | acc])
+  defp needed_fuel(mass) do
+    (div(mass, 3) - 2)
+    |> max(0)
   end
 end
